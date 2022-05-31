da976b3cc34dbbc03574dae778ab4c5d62c821f0
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-05-30 23:22:31 -0300

    purchase success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,11 +1,29 @@
+import { Button, Flex, Text } from "@chakra-ui/react"
 import { useRouter } from "next/router"
+import { FaCheckCircle } from "react-icons/fa"
+import { Container } from "../components/Container"
 
 export default function Success() {
-  const { query } = useRouter()
+  const { query, push } = useRouter()
 
   return (
-    <div>
-      <h1>Parabéns! Você adquiriu o produto {query.productName}</h1>
-    </div>
+    <Container>
+      <Flex h="80vh" flexDir="column" align="center" justify="center">
+        <FaCheckCircle size={100} color="green" />
+        <Text mt="2rem" textAlign="center" fontSize="xl">
+          <strong>Congratulations!</strong> <br /> You just bought a{" "}
+          {query.productName}
+        </Text>
+        <Button
+          mt="1rem"
+          bg="gray.800"
+          color="gray.50"
+          _hover={{ background: "gray.800" }}
+          onClick={() => push("/products")}
+        >
+          Back to products page
+        </Button>
+      </Flex>
+    </Container>
   )
 }
